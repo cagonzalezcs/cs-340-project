18707c5647e9cc1e11431e75e86951dfcf3c6518
cagonzalezcs <cgonzalez@honorlock.com>
2022-05-19 16:04:47 -0500

    Fixing issue with const variable reference

--- server/routes/books.mjs
+++ server/routes/books.mjs
@@ -38,15 +38,15 @@ booksRouter.get('/:id', async (request, response) => {
     try {
         const newBookData = request.body;
         console.log('tuptop')
-        const genre_id = parseInt(request.body.genre_id);
-        if (isNaN(genre_id))
+        let genre_id = Number(request.body.genre_id);
+        if (!genre_id)
         {
             genre_id = 'NULL'
         }
         await createBook(
             newBookData.title,
             newBookData.author,
-            newBookData.genre_id,
+            genre_id,
             newBookData.isbn,
             newBookData.cover_image,
             newBookData.quantity_available,
