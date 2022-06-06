abcfd47200a1f1fbae3a8fd5cf0946241e1060e0
dlyonskeefe <dlyonskeefe@gmail.com>
2022-06-06 13:16:21 -0700

    group_28_ddm.sql file update

--- sql-dumps/group-28-ddm.sql
+++ sql-dumps/group-28-ddm.sql
@@ -5,9 +5,74 @@
 INSERT INTO users (user_role_id, first_name, last_name, email, address_line_1, address_line_2, city, state, password)
 VALUES(:user_role_id, :user_first_name, :user_last_name, :user_email, :user_address_line_1, :user_address_line_2, :user_city, :user_state, :user_password),
 
--- get all users to display on UsersPage.vue
-SELECT
-	users.id,
+-- get all Users
+SELECT users.id,
+    users.user_role_id,
+    users.first_name,
+    users.last_name,
+    users.email,
+    users.address_line_1,
+    users.address_line_2,
+    users.city,
+    users.state,
+    users.created_at,
+    users.updated_at
+FROM users
+
+-- get User by Id (with and without password)
+-- with password
+SELECT users.id,
+    users.user_role_id,
+    users.first_name,
+    users.last_name,
+    users.email,
+    users.address_line_1,
+    users.address_line_2,
+    users.city,
+    users.state,
+    users.password,
+    users.password_updated_at,
+    users.created_at,
+    users.updated_at
+FROM users
+WHERE users.id = :user_id_input;
+LIMIT 1
+-- without password
+SELECT users.id,
+    users.user_role_id,
+    users.first_name,
+    users.last_name,
+    users.email,
+    users.address_line_1,
+    users.address_line_2,
+    users.city,
+    users.state,
+    users.created_at,
+    users.updated_at
+FROM users
+WHERE users.id = :user_id_input;
+LIMIT 1
+
+-- get User by Email (with and without password)
+-- with password
+SELECT users.id,
+    users.user_role_id,
+    users.first_name,
+    users.last_name,
+    users.email,
+    users.address_line_1,
+    users.address_line_2,
+    users.city,
+    users.state,
+    users.password,
+    users.password_updated_at,
+    users.created_at,
+    users.updated_at
+FROM users
+WHERE users.email = :user_email_input;
+LIMIT 1
+-- without password
+SELECT users.id,
     users.user_role_id,
     users.first_name,
     users.last_name,
@@ -16,16 +81,33 @@ SELECT
     users.address_line_2,
     users.city,
     users.state
-FROM
-    users;
+FROM users
+WHERE users.email = :user_email_input;
+LIMIT 1
 
--- get single User id, email, and password to verify and log user in
-SELECT
-    users.id,
-    users.email,
-    users.password
-FROM
-    users;
+-- create User
+INSERT INTO users(
+    user_role_id, 
+    first_name, 
+    last_name, 
+    email, 
+    address_line_1, 
+    address_line_2, 
+    city, 
+    state,
+    password
+)
+VALUES (
+    :user_role_id,
+    :user_first_name, 
+    :user_last_name, 
+    :user_email, 
+    :user_address_line_1, 
+    :user_address_line_2, 
+    :user_city, 
+    :user_state, 
+    :user_encrypted_password
+)
 
 -- update single User to handle updates from admin
 UPDATE
@@ -42,66 +124,139 @@ SET
 WHERE
         users.id = :user_id_value;
 
--- Update user password
-UPDATE
-    users
-SET
-    users.password = :password_input
-    WHERE
-            users.id = :user_id_value;
+-- delete User
+DELETE FROM users WHERE user.id = :user_id_value;
 
--- delete single User to handle deletion from admin
-DELETE FROM users
-WHERE user.id = :user_id_value;
-
-
--- user_role queries
--- get user role to check if the user is an admin or customer
+-- get User Rental List
 SELECT
-    user_roles.type,
-    users.id
-FROM user_roles
-         INNER JOIN users ON users.user_role_id = user_roles.id
-WHERE
-        users.id = :user_id_input;
-
-
-
--- rental_list_books queries
--- get all rental list books for a user
-SELECT
-    books.id,
-    books.title,
-    books.isbn
+    rental_list_books.user_id AS user_id,
+    books.id, 
+    books.title
 FROM
     books
-        INNER JOIN rental_list_books ON books.id = rental_list_books.book_id
-        INNER JOIN users ON rental_list_books.user_id = users.id
+INNER JOIN rental_list_books ON books.id = rental_list_books.book_id
 WHERE
-        user_id = :user_id_input;
+    rental_list_books.user_id = :user_id_value;
+
+-- get User Wish List
+SELECT
+    wish_list_books.user_id AS user_id,
+    books.id,
+    books.title
+FROM
+    books
+INNER JOIN wish_list_books ON books.id = wish_list_books.book_id
+WHERE
+    wish_list_books.user_id = :user_id_value;
+
+-- user_role queries
+-- get all user roles
+SELECT *
+FROM user_roles;
+
+-- get user role by ID
+SELECT *
+FROM user_roles
+WHERE
+    users.id = :user_id_input;
+LIMIT 1;
+
+-- create user role
+INSERT INTO user_roles(type)
+VALUES(:role_type_input);
+
+-- update user role
+UPDATE user_roles
+SET user_roles.type = :updated_user_role_type;
+WHERE user_roles.id = :user_role_id;
+
+-- delete user role
+DELETE FROM user_roles WHERE user_roles.id = :user_role_id;
+
+-- rental_list_books queries
+-- get all rental lists
+SELECT
+    rental_list_books.user_id AS user_id, 
+    books.id,
+    books.title
+FROM
+    books
+INNER JOIN rental_list_books ON books.id = rental_list_books.user_id
+GROUP BY
+    rental_list_books.user_id
+ORDER BY
+    rental_list_books.user_id;
+
+-- get all rental list items for a user
+SELECT rental_list_books.book_id,
+    books.title,
+    books.isbn,
+    books.cover_image,
+    (SELECT GROUP_CONCAT(authors.name SEPARATOR ', ')
+FROM authors
+LEFT JOIN book_authors ON authors.id = book_authors.author_id
+WHERE book_authors.book_id = rental_list_books.book_id) AS 'authors',
+    genres.name AS 'genre',
+    rental_list_books.created_at AS 'date_rented'
+FROM rental_list_books
+LEFT JOIN books ON rental_list_books.book_id = books.id
+LEFT JOIN book_authors ON rental_list_books.book_id = book_authors.book_id
+LEFT JOIN authors ON book_authors.author_id = authors.id
+LEFT JOIN genres ON books.genre_id = genres.id
+WHERE rental_list_books.user_id = :user_id_input;
+GROUP BY rental_list_books.book_id
+ORDER BY date_rented DESC;
 
 -- add a book to the user's rental list
 INSERT INTO rental_list_books (user_id, book_id)
 VALUES(:user_id_input, :book_id_input);
 
 -- remove a book from the user's rental list
-DELETE FROM rental_list_books
+DELETE 
+FROM rental_list_books
 WHERE rental_list_books.user_id = :user_id_input
   AND rental_list_books.book_id = :book_id_input;
 
 
 -- wish_list_books queries
--- get all wish list books for a user
+-- get all wish lists
 SELECT
+    wish_list_books.user_id AS user_id, 
     books.id,
-    books.title,
-    books.isbn
+    books.title
 FROM
     books
-        INNER JOIN wish_list_books ON books.id = rental_list_books.book_id
-        INNER JOIN users ON wish_list_books.user_id = users.id
-WHERE
-        user_id = :user_id_input;
+INNER JOIN wish_list_books ON books.id = wish_list_books.user_id
+GROUP BY
+    wish_list_books.user_id
+ORDER BY
+    wish_list_books.user_id;
+
+-- get a single wish list item
+SELECT *
+FROM wish_list_books
+WHERE user_id = :user_id_input
+    AND book_id = :book_id_input;
+
+-- get all wish list books for a user
+SELECT wish_list_books.book_id,
+    wish_list_books.created_at,
+    books.title,
+    books.isbn,
+    books.cover_image,
+    (SELECT GROUP_CONCAT(authors.name SEPARATOR ', ')
+FROM authors
+LEFT JOIN book_authors ON authors.id = book_authors.author_id
+WHERE book_authors.book_id = wish_list_books.book_id) AS 'authors',
+    genres.name AS 'genre'
+FROM wish_list_books
+LEFT JOIN books ON wish_list_books.book_id = books.id
+LEFT JOIN book_authors ON wish_list_books.book_id = book_authors.book_id
+LEFT JOIN authors ON book_authors.author_id = authors.id
+LEFT JOIN genres ON books.genre_id = genres.id
+WHERE wish_list_books.user_id = ?
+GROUP BY wish_list_books.book_id
+ORDER BY wish_list_books.created_at DESC;
 
 -- add a book to the user's wish list
 INSERT INTO wish_list_books (user_id, book_id)
@@ -113,81 +268,72 @@ WHERE wish_list_books.user_id = :user_id_input
   AND wish_list_books.book_id = :book_id_input;
 
 
-
 -- Genre queries 
--- get all Genres to display on GenresPage.vue
-SELECT
-    genres.id,
-    genres.name
-FROM
-    genres; 
+-- get all Genres
+SELECT *
+FROM genres
+ORDER BY genres.id;
+
+-- get page of Genres
+SELECT *
+FROM genres
+ORDER BY genres.id
+LIMIT :input_page_num, :input_per_page;
+
+-- get genre by ID
+SELECT *
+FROM genres
+WHERE genres.id = :genre_id_from_genres_page;
+
+-- get genre ID by name
+SELECT genres.id
+FROM genres
+WHERE genres.name = :genre_name_input;
 
 -- add new Genre
 INSERT INTO genres(NAME)
-VALUES(:genre_name_input)
-
--- get single Genre to update in updateGenre form
-SELECT
-    genres.id,
-    genres.name
-FROM
-    genres
-WHERE
-    genres.id = :genre_id_from_genres_page;
+VALUES(:genre_name_input);
 
 -- update Genre based on genre_id selection from the updateGenre form
-UPDATE
-    genres
-SET
-    genres.name = :genre_name_input
-WHERE
-    genres.id = :genre_id_from_genres_page;
+UPDATE genres
+SET genres.name = :genre_name_input
+WHERE genres.id = :genre_id_from_genres_page;
 
 -- delete Genre
-DELETE
-FROM
-    genres
-WHERE
-    genres.id = :genre_id_from_genres_page;
+DELETE FROM genres
+WHERE genres.id = :genre_id_from_genres_page;
 -- end Genre queries
 
 
 -- Author queries
 -- get all Authors to display on AuthorsPage.vue
-SELECT 
-    authors.name, 
-    authors.birth_date 
-FROM 
-    authors; 
+SELECT authors.id, authors.name, TRIM('T07:00:00.000Z' FROM authors.birth_date) as 'birth_date'
+FROM authors
+ORDER BY authors.id; 
+
+-- get single Author by ID
+SELECT authors.id, authors.name, TRIM('T07:00:00.000Z' FROM authors.birth_date) as 'birth_date'
+FROM authors
+WHERE authors.id = :author_id_input
+ORDER BY authors.id;
+
+-- get author ID by name
+SELECT authors.id 
+FROM authors 
+WHERE authors.name = :author_name_input;
 
 -- add new Author based on items from addAuthor form
 INSERT INTO authors (name, birth_date) 
 VALUES(:name_input, :birth_date_input);
 
--- get single Author to update for updateAuthor form
-SELECT
-    authors.name,
-    authors.birth_date
-FROM
-    authors
-WHERE
-    authors.id = :author_id_from_authors_page;
-
 -- update Author based on selection from updateAuthor form
-UPDATE
-    authors
-SET
-    authors.name = :name_input,
-    authors.birth_date = :birth_date_input
-WHERE
-    authors.id = :author_id_from_authors_page;
+UPDATE authors
+SET authors.name = :name_input, authors.birth_date = :birth_date_input
+WHERE authors.id = :author_id;
 
 -- delete Author 
-DELETE
-FROM
-    authors
-WHERE
-    authors.id = :author_id_from_authors_page;
+DELETE FROM authors
+WHERE authors.id = :author_id;
 -- end Author queries
 
 
@@ -203,16 +349,19 @@ INNER JOIN
 	book_authors ON books.id = book_authors.book_id 
 INNER JOIN 
 	authors ON book_authors.author_id = authors.id
-WHERE author_id = :author_id_from_authors_page;
+WHERE author_id = :author_id;
 -- end Book_Author queries
 
 
 -- Book queries 
--- show Books, their authors.name and genres.name
+-- get all Books for books page
 SELECT 
     books.id, 
     books.title,  
-    authors.name as author,
+    (SELECT GROUP_CONCAT(authors.name SEPARATOR ', ')
+    FROM authors
+    LEFT JOIN book_authors ON authors.id = book_authors.author_id
+    WHERE book_authors.book_id = books.id) AS authors,
     genres.name AS genre, 
     books.isbn, 
     books.cover_image, 
@@ -220,17 +369,17 @@ SELECT
     books.quantity_rented 
 FROM 
     books 
-INNER JOIN 
-    genres ON books.genre_id = genres.id
-INNER JOIN 
-    book_authors ON books.id = book_authors.book_id
-INNER JOIN
-	authors ON book_authors.author_id = authors.id
-ORDER BY 
-	books.id ASC;
+LEFT JOIN genres ON books.genre_id = genres.id
+LEFT JOIN book_authors ON books.id = book_authors.book_id
+LEFT JOIN authors ON book_authors.author_id = authors.id
+GROUP BY books.id
+ORDER BY books.id;
 
--- get all genre_ids and their names to populate the Genre dropdown
-SELECT genres.id, genres.name FROM genres; 
+-- get a single book by ID
+SELECT * 
+FROM books 
+WHERE books.id = :book_id_input
+LIMIT 1;
 
 -- add new Book based on items from addBook form 
 INSERT INTO books(
@@ -243,37 +392,23 @@ INSERT INTO books(
 )
 VALUES(
     :title_input,
-    (SELECT genres.id FROM genres WHERE name=:genre_input_from_drop_down),
+    :genre_id_input,
     :isbn_input,
     :cover_image_input,
     :quantity_available_input,
     :quantity_rented_input
 );
 -- complete book add by associating it with an Author, and inserting that into book_authors
+SELECT author.id FROM authors WHERE authors.name = :author_name_input 
+
 INSERT INTO book_authors(
     book_id, 
     author_id
 )
 VALUES(
     :book_id_input,
-    (SELECT id FROM authors WHERE name=:author_name_from_drop_down)
+    :author.id
 );
-
--- get single Book data for update form
-SELECT 
-    books.id, 
-    books.title,  
-    genres.name AS genre, 
-    books.isbn, 
-    books.cover_image, 
-    books.quantity_available, 
-    books.quantity_rented 
-FROM 
-    books 
-INNER JOIN 
-    genres ON books.genre_id = genres.id
-WHERE 
-    books.id= :book_id_from_books_page;
 
 -- update this Book 
 UPDATE 
@@ -287,64 +422,99 @@ SET
     books.quantity_rented= :quantity_rented_input
 WHERE 
     books.id= :book_id_from_books_page;
-
--- update book_authors
--- complete book add by associating it with an Author, and inserting that into book_authors
-UPDATE 
-    book_authors
-SET 
-    author_id = (SELECT id FROM authors WHERE name=:author_name_from_drop_down)
-WHERE 
-    book_authors.book_id= :book_id_from_books_page;
+-- delete entry from book_authors
+DELETE FROM book_authors 
+WHERE book_id = :book_id_input;
+-- get new authord ID for book authors
+SELECT authors.id FROM authors WHERE authors.name = :author_name_input
+--update book authors
+INSERT INTO book_authors (book_authors.book_id, book_authors.author_id)
+VALUES (:book_id_input, :author_id_input)
 
 -- delete a Book 
-DELETE 
-FROM 
-    books   
-WHERE 
-    books.id= :book_id_from_books_page;
--- end Books queries 
+DELETE FROM books   
+WHERE books.id= :book_id_from_books_page;
 
 
 -- Search Books queries
-
--- select Genres for search dropdown
-SELECT genre.id, genre.name FROM genres; 
-
 -- search by Genre
-SELECT
+SELECT books.id,
     books.title,
-    authors.name,
-    books.isbn
-FROM
-    books
-LEFT JOIN book_authors ON book_authors.book_id = books.id
+    (SELECT GROUP_CONCAT(authors.name SEPARATOR ', ')
+    FROM authors
+            LEFT JOIN book_authors ON authors.id = book_authors.author_id
+    WHERE book_authors.book_id = books.id) AS "authors",
+    genres.name AS "genre",
+    books.isbn,
+    books.cover_image,
+    books.quantity_available,
+    books.quantity_rented
+FROM books
+LEFT JOIN genres ON books.genre_id = genres.id
+LEFT JOIN book_authors ON books.id = book_authors.book_id
 LEFT JOIN authors ON book_authors.author_id = authors.id
-WHERE books.genre_id= :book_genre_id_from_genre_dropdown;
+WHERE books.genre_id = :genre_id_input
+GROUP BY books.id
+ORDER BY books.id;
 
 -- search by Title
-SELECT
+SELECT books.id,
     books.title,
-    authors.name,
-    books.isbn
-FROM
-    books
-LEFT JOIN book_authors ON book_authors.book_id = books.id
+    (SELECT GROUP_CONCAT(authors.name SEPARATOR ', ')
+    FROM authors
+            LEFT JOIN book_authors ON authors.id = book_authors.author_id
+    WHERE book_authors.book_id = books.id) AS "authors",
+    genres.name AS "genre",
+    books.isbn,
+    books.cover_image,
+    books.quantity_available,
+    books.quantity_rented
+FROM books
+LEFT JOIN genres ON books.genre_id = genres.id
+LEFT JOIN book_authors ON books.id = book_authors.book_id
 LEFT JOIN authors ON book_authors.author_id = authors.id
-WHERE books.title= :book_title_input;
-
--- select Authors for search dropdown
-SELECT authors.id, authors.name FROM genres;
-
+WHERE books.title LIKE :book_title_search_input
+GROUP BY books.id
+ORDER BY books.id;
 
 -- search by Author
-SELECT
+SELECT books.id,
     books.title,
-    authors.name,
-    books.isbn
-FROM
-    books
-LEFT JOIN book_authors ON book_authors.book_id = books.id
+    (SELECT GROUP_CONCAT(authors.name SEPARATOR ', ')
+    FROM authors
+            LEFT JOIN book_authors ON authors.id = book_authors.author_id
+    WHERE book_authors.book_id = books.id) AS "authors",
+    genres.name AS "genre",
+    books.isbn,
+    books.cover_image,
+    books.quantity_available,
+    books.quantity_rented
+FROM books
+LEFT JOIN genres ON books.genre_id = genres.id
+LEFT JOIN book_authors ON books.id = book_authors.book_id
 LEFT JOIN authors ON book_authors.author_id = authors.id
-WHERE authors.name= :author_name_input;
+WHERE book_authors.author_id = :author_id_input
+GROUP BY books.id
+ORDER BY books.id;
+
+-- search by Keyword
+SELECT books.id,
+    books.title,
+    (SELECT GROUP_CONCAT(authors.name SEPARATOR ', ')
+    FROM authors
+            LEFT JOIN book_authors ON authors.id = book_authors.author_id
+    WHERE book_authors.book_id = books.id) AS "authors",
+    genres.name AS "genre",
+    books.isbn,
+    books.cover_image,
+    books.quantity_available,
+    books.quantity_rented
+FROM books
+LEFT JOIN genres ON books.genre_id = genres.id
+LEFT JOIN book_authors ON books.id = book_authors.book_id
+LEFT JOIN authors ON book_authors.author_id = authors.id
+WHERE books.title LIKE :keyword_input
+OR genres.name LIKE :keyword_input
+OR authors.name LIKE :keyword_input
+GROUP BY books.id;
 -- end Search Books queries
